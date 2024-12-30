789636ac9e855ef382a8d5cc0f5cab932cfc0907
Miguel <miguel.kuhr@gmail.com>
2024-12-30 12:58:17 +1030

    path added

---
+++ front-e-commerce/src/components/Path.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="d-flex">
+        <p style="opacity: 0.5;" v-for="oldP in oldPaths" :key="oldP">&nbsp;&nbsp;{{oldP}}&nbsp;&nbsp; /</p>
+        <span>&nbsp;&nbsp; {{activePath}}</span>
+    </div>
+</template>
+      
+  <script setup>
+      defineProps({
+          oldPaths: {
+              type: Array,
+              default: null
+          },
+          activePath:{
+            type: String,
+            default: null
+          }
+      })
+  </script>
+      
+  <style scoped>
+  body {
+    font-family: "Poppins", serif;
+    font-size: 14px;
+  }
+  </style>
+      
--- front-e-commerce/src/pages/Profile.vue
+++ front-e-commerce/src/pages/Profile.vue
@@ -1,9 +1,11 @@
 <template>
     <Header/>
+    <Path :oldPaths="['Home']" activePath="Minha Conta" />
 </template>
   
 <script setup>
     import Header from '@/components/Header.vue';
+    import Path from '@/components/Path.vue';
 </script>
   
   <style scoped>
