f8e41b8fcb021233ccf09ee8ee8fac4e3efd74b5
Miguel <miguel.kuhr@gmail.com>
2025-01-07 22:05:09 +1030

    finished cart page

--- front-e-commerce/src/pages/Cart.vue
+++ front-e-commerce/src/pages/Cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <Header :cartActive="true" />
+  <Header :cart-active="true" />
   <v-divider />
   <div class="container">
     <Path
@@ -16,7 +16,7 @@
           <span>Produto</span>
         </v-col>
         <v-col class="d-flex justify-center align-center">
-          <span>Preco</span>
+          <span>Preço</span>
         </v-col>
         <v-col class="d-flex justify-center align-center">
           <span>Quantidade</span>
@@ -32,7 +32,14 @@
         justify="center"
       >
         <v-col class="d-flex justify-center align-center">
-          <span>{{ cartProduct.name }}</span>
+          <div class="d-flex  justify-center align-center ga-6">
+            <v-img
+              rounded="lg"
+              width="54"
+              height="54"
+              :src="cartProduct.image"
+            /><span>{{ cartProduct.name }}</span>
+          </div>
         </v-col>
         <v-col class="d-flex justify-center align-center">
           <span>R$ {{ cartProduct.price }}</span>
@@ -44,16 +51,75 @@
           <span>R$ {{ cartProduct.price * cartProduct.quantity }}</span>
         </v-col>
       </v-row>
+      <v-row style="margin-bottom: 80px;">
+        <button class="button">
+          Retornar à busca
+        </button>
+        <v-spacer />
+        <button class="button">
+          Atualizar carrinho
+        </button>
+      </v-row>
+      <v-row align="start">
+        <v-col class="d-flex justify-center align-center">
+          <v-row class="ga-4"> 
+            <Input
+              hide-details="true"
+              variant="outlined"
+              :max-width="300"
+              label="Codigo de cupom"
+            />
+            <button class="button golden">
+              Aplicar cupom
+            </button>
+          </v-row>
+        </v-col>
+        <v-spacer />
+        <v-col class="cart-total">
+          <v-row class="mb-6">
+            <h1>Cart Total</h1>
+          </v-row>
+          <v-row class="mb-4">
+            <h2>Subtotal:</h2> <v-spacer /> R$ 1750
+          </v-row>
+          <v-row class="mb-4">
+            <v-divider
+              class="border-opacity-50"
+              color="#000"
+            />
+          </v-row>
+          <v-row class="mb-4">
+            <h2>Frete:</h2> <v-spacer /> Free
+          </v-row>
+          <v-row class="mb-4">
+            <v-divider
+              class="border-opacity-50"
+              color="#000"
+            />
+          </v-row>
+          <v-row class="mb-4">
+            <h2>Total:</h2> <v-spacer /> R$ 1750
+          </v-row>
+          <v-row justify="center">
+            <button class="button golden">
+              Avançar para o checkout
+            </button>
+          </v-row>
+        </v-col>
+      </v-row>
     </div>
   </div>
   <Footer />
 </template>
     
 <script setup>
+  import card1 from '@/assets/card1.webp'
+  import card2 from '@/assets/card2.webp'
+
     import { ref } from 'vue'
     import Header from '@/core/Header.vue';
     import Path from '@/core/Path.vue';
-    //import Input from  '@/core/Input.vue';
+    import Input from  '@/core/Input.vue';
     import Footer from '@/core/Footer.vue';
 
     const cartProducts = ref([{
@@ -61,23 +127,26 @@
       price: 650,
       quantity: 1,
       subtotal: 650,
+      image: card1,
     },
     {
       name: 'LCD Monitor',
       price: 650,
       quantity: 1,
       subtotal: 650,
+      image: card2,
     }])
 </script>
     
 <style scoped>
 .container {
+  font-family: "Poppins", serif;
   margin-top: 80px;
   padding-right: 135px;
   padding-left: 135px;
 }
 .row {
-  padding: 24px 24px 24px 24px;
+  padding: 12px;
   border-radius: 4px;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
 }
@@ -86,10 +155,37 @@ span {
 }
 .table {
   width: 100%;
+  margin-bottom: 140px;
 
   display: flex;
   flex-direction: column;
   gap: 30px;
+}
+.button {
+  font-size: 16px;
+  border: 1px solid #000;
+  border-radius: 4px;
+  padding: 16px 48px;
+  color: #000;
+  background-color: #FFF;
+}
+.golden {
+  background-color: #DBB671;
+  color: white;
+  border: none;
+}
+.cart-total {
+  border: 2px solid #000;
+  border-radius: 4px;
+  padding: 32px 24px 32px 24px;
+}
+h1 {
+  font-size: 20px;
+  font-weight: 500;
+}
+h2 {
+  font-weight: 400;
+  font-size: 16px;
 }
 </style>
     
