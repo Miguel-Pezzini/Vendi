6022a822f7ba9d54e1430018d9f10a1ef20aeb79
Miguel <miguel.kuhr@gmail.com>
2025-01-08 21:57:24 +1030

    formatting product card

--- front-e-commerce/src/pages/Product.vue
+++ front-e-commerce/src/pages/Product.vue
@@ -3,35 +3,74 @@
     <Header />
     <v-divider />
 
-    <div class="container" @click="loadPaths">
+    <div
+      class="container"
+      @click="loadPaths"
+    >
       <v-row>
         <Path
-        :old-paths="oldPaths"
-        :active-path="prod.name"
+          :old-paths="oldPaths"
+          :active-path="prod.name"
         />
       </v-row>
 
       <v-row style="margin-top: 80px;">
         <v-col>
-          <div class="d-flex">
-            <div class="d-flex flex-column">
-              <img v-for="i in 4" :key="i"
+          <div class="d-flex product-container ga-8">
+            <div class="d-flex flex-column ga-4">
+              <v-img
+                v-for="i in 4"
+                :key="i"
+                aspect-ratio="16/9"
+                cover
                 :src="card1"
-                width="138"
-                height="170"
+                :height="calcularHeightImg(4)"
+                width="170"
               />
             </div>
             <div style="max-width: 500px;">
-              <img
-              class="fill-height fill-width"
+              <v-img
+                aspect-ratio="1/1"
+                cover
+                class="fill-height fill-width"
                 :src="card1"
+                :height="heightBigImg"
                 width="500"
               />
             </div>
           </div>
         </v-col>
+        <v-col class="ml-16">
+          <div class="d-flex flex-column">
+            <div class="d-flex flex-column ga-4">
+              <h1>{{ prod.name }}</h1>
+              <div class="d-flex ga-4">
+                <div class="d-flex ga-2">
+                  <v-rating
+                    :size="24"
+                    active-color="yellow"
+                    :model-value="prod.rating"
+                    readonly
+                  />
+                  <p
+                    v-if="prod.reviews"
+                    style="opacity: 0.5;"
+                  >
+                    ({{ prod.reviews }} Avaliações)
+                  </p>
+                </div>
+                <p style="opacity: 0.5;">
+                  |
+                </p>
+                <span :style="{ color: prod.inStock ? '#00FF66' : '#FF0000' }">
+                  {{ prod.inStock ? 'Em Estoque' : 'Indisponível' }}
+                </span>
+              </div>
+              <h2>R$ {{ prod.price }}</h2>
+            </div>
+          </div>
+        </v-col>
       </v-row>
-    
     </div>
      
     <Footer />
@@ -42,7 +81,7 @@
   import card1 from '@/assets/card1.webp'
   // import card2 from '@/assets/card2.webp'
   // import card3 from '@/assets/card3.webp'
-  // import card4 from '@/assets/card4.webp'
+  //  import card4 from '@/assets/card4.webp'
 
   import { useRoute } from 'vue-router';
   import { ref, onMounted } from 'vue'
@@ -53,11 +92,17 @@
   //import router from '@/router';
   const oldPaths = ref([])
 
+  const heightBigImg = 600;
+
   const route = useRoute()
 
   onMounted(() => {
     oldPaths.value = loadPastPaths(route)
   })
+  
+  function calcularHeightImg(timeImages) {
+    return (heightBigImg / timeImages) - 64
+  }
 
   const prod = ref({
     discount: 35,
@@ -65,6 +110,9 @@
     price: "960",
     fullPrice: "1160",
     isInWishList: true,
+    rating: 4,
+    reviews: 150,
+    inStock: true,
   })
 
   </script>
@@ -80,6 +128,10 @@
     margin-bottom: 140px;
     margin-right: 135px;
     margin-left: 135px;
+  }
+  .product-container {
+    max-height: 600px;
+    max-width: 800px ;
   }
   </style>
   
