b8becc3fb16f099c12d70debde9fea433f9e853f
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-02-15 13:42:12 -0300

    fix: Arruma global snack bar

--- front-e-commerce/src/core/plugins/GlobalSnackbar.vue
+++ front-e-commerce/src/core/plugins/GlobalSnackbar.vue
@@ -1,12 +1,14 @@
 <template>
-    <v-snackbar v-model="snackbarData.visible" :color="snackbarData.color" top :timeout="3000">
+    <v-snackbar location="right | top" v-model="snackbarData.visible" :color="snackbarData.color" :timeout="3000">
       {{ snackbarData.message }}
-      <v-icon
+          <template v-slot:actions>
+            <v-icon
             @click="snackbarData.visible = false"
             size="x-large"
             :color="shoppingActive ? '#DBB671' : null"
             icon="mdi-close"
           />
+      </template>
     </v-snackbar>
   </template>
   
