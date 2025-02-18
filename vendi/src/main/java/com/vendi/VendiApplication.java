d841b77f82076bb131fbe2518d973f2b2f64f7ca
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-02-18 20:21:23 -0300

    fix: Adiciona Auditing para operações de edição

--- vendi/src/main/java/com/vendi/VendiApplication.java
+++ vendi/src/main/java/com/vendi/VendiApplication.java
@@ -2,8 +2,10 @@ package com.vendi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VendiApplication {
 
 	public static void main(String[] args) {
---
+++ vendi/src/main/java/com/vendi/infra/security/CustomAuditorAware.java
@@ -0,0 +1,28 @@
+package com.vendi.infra.security;
+
+import com.vendi.domain.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class CustomAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Se o usuário estiver autenticado
+        if (authentication != null && authentication.isAuthenticated()) {
+            User user = (User) authentication.getPrincipal();
+            return Optional.of(user.getId().toString());  // Retorna o nome de usuário
+        }
+
+        // Caso não esteja autenticado, retorna um valor default ou null
+        return Optional.empty();
+    }
+}
