58e652151593b374e4c167ae9690316b6efc9ac8
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-03-16 17:00:49 -0300

    feat: Add ResourceNotFoundException and global handling for all exceptions

--- vendi/src/main/java/com/vendi/exceptions/GlobalExceptionHandler.java
+++ vendi/src/main/java/com/vendi/exceptions/GlobalExceptionHandler.java
@@ -15,9 +15,23 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<String> handleResourceAlreadyExists(ResourceAlreadyExistsException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(e.getMessage());
     }
 }
