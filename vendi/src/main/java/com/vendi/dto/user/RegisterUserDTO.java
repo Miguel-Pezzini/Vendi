7fb676d3f1499ad42a0b0c48c701545c7a78c161
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-03-15 22:31:19 -0300

    feat: Create validations to registerUserDTO

--- vendi/src/main/java/com/vendi/dto/user/RegisterUserDTO.java
+++ vendi/src/main/java/com/vendi/dto/user/RegisterUserDTO.java
@@ -1,6 +1,12 @@
 package com.vendi.dto.user;
 
 import com.vendi.model.user.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
-public record RegisterUserDTO(String name, String email, String password, UserRole role) {
+public record RegisterUserDTO(
+        @NotNull String name,
+        @NotNull @Email String email,
+        @NotNull String password,
+        @NotNull UserRole role) {
 }
