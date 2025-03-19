aa3b770b744f0f4fba9a1f72b126af991d910953
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-03-18 22:05:24 -0300

    feat: Add Cart

---
+++ vendi/src/main/java/com/vendi/model/cart/CartItem.java
@@ -0,0 +1,30 @@
+package com.vendi.model.cart;
+
+import com.vendi.model.AbstractEditableEntity;
+import com.vendi.model.AbstractEditableEntity;
+import com.vendi.model.product.Product;
+import com.vendi.model.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_item")
+public class CartItem extends AbstractEditableEntity {
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Min(1)
+    private int quantity;
+}
