d1788a430513b782d07e0fc8d34fceed2b0c388f
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-03-18 22:04:33 -0300

    fix: Remove todo

--- vendi/src/main/java/com/vendi/controller/ProductController.java
+++ vendi/src/main/java/com/vendi/controller/ProductController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-// TODO IMPLEMENT ENTITYGRAPHS
 @RestController
 @RequestMapping("/product")
 public class ProductController {
