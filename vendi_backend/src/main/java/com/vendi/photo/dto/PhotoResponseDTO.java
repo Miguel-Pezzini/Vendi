ea755a4eb51560c175947e1fb70668c68ef824ac
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-06-22 01:16:30 -0300

    feat: Add isMain to PhotoResponseDTO

--- vendi_backend/src/main/java/com/vendi/photo/dto/PhotoResponseDTO.java
+++ vendi_backend/src/main/java/com/vendi/photo/dto/PhotoResponseDTO.java
@@ -4,8 +4,8 @@ import com.vendi.photo.model.Photo;
 
 import java.util.UUID;
 
-public record PhotoResponseDTO(UUID id, String contentType, String filename) {
+public record PhotoResponseDTO(UUID id, String contentType, String filename, Boolean isMainPhoto) {
     public PhotoResponseDTO(Photo photo) {
-        this(photo.getId(), photo.getContentType(), photo.getFilename());
+        this(photo.getId(), photo.getContentType(), photo.getFilename(), photo.getIsMain());
     }
 }
