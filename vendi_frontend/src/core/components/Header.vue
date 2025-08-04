0e3d67c94f4ce37c50b6053069ac3b392b49723e
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-08-03 22:53:05 -0300

    feat: Creates Icon component

--- vendi_frontend/src/core/components/Header.vue
+++ vendi_frontend/src/core/components/Header.vue
@@ -12,32 +12,17 @@
     </v-col>
   </v-row>
   <v-row class="header-container" align="center" no-gutters>
-    <v-col cols="3" class="d-flex justify-center align-center">
+    <v-col cols="2" class="d-flex justify-center align-center">
       <router-link to="/home">
         <img class="img" src="@/assets/logo.png" alt="Logo" />
       </router-link>
     </v-col>
-    <v-col cols="1" class="d-flex justify-center">
-      <RouterLink :to="{ path: '/store', query: { category: 'most-seller', origin: ['Home'] } }">
-        Mais Vendidos
+    <v-col v-for="category in categories" class="d-flex justify-center">
+      <RouterLink :to="{ path: `${category.path}`, query: category.category }">
+        {{ category.name }}
       </RouterLink>
     </v-col>
-    <v-col cols="1" class="d-flex justify-center">
-      <RouterLink :to="{ path: '/store', query: { category: 'eletronics', origin: ['Home'] } }">
-        Eletrônicos
-      </RouterLink>
-    </v-col>
-    <v-col cols="1" class="d-flex justify-center">
-      <RouterLink :to="{ path: '/store', query: { category: 'household', origin: ['Home'] } }">
-        Domésticos
-      </RouterLink>
-    </v-col>
-    <v-col cols="1" class="d-flex justify-center">
-      <RouterLink :to="{ path: '/store', query: { category: 'books', origin: ['Home'] } }">
-        Livros
-      </RouterLink>
-    </v-col>
-    <v-col cols="2" class="d-flex justify-center align-center ml-8">
+    <v-col cols="2" class="d-flex justify-center align-center">
       <form style="width: 100%" @submit.prevent="pesquisar">
         <Input
           v-model="dadoPesquisa"
@@ -48,38 +33,45 @@
           @click:append-inner="pesquisar()" />
       </form>
     </v-col>
-    <v-col>
+    <v-col cols="2">
       <v-row class="d-flex justify-center ga-4">
-        <RouterLink :to="{ path: '/wishlist', query: { origin: ['Home'] } }">
-          <v-icon
+        <RouterLink :to="{ path: '/wishlist' }">
+          <Icon
             size="x-large"
-            :color="wishListActive ? '#DBB671' : null"
+            tooltip="Wish List"
+            :color="wishListActive ? 'golden' : null"
             :icon="wishListActive ? 'mdi-heart' : 'mdi-heart-outline'" />
         </RouterLink>
         <RouterLink>
-          <v-icon
+          <Icon
             size="x-large"
-            :color="shoppingActive ? '#DBB671' : null"
+            tooltip="My Orders"
+            :color="shoppingActive ? 'golden' : null"
             icon="mdi-shopping-outline" />
         </RouterLink>
 
         <RouterLink>
-          <v-icon
+          <Icon
             size="x-large"
-            :color="cartActive ? '#DBB671' : null"
+            :color="cartActive ? 'golden' : null"
             icon="mdi-cart-outline"
+            tooltip="Cart"
             @click="showCart = !showCart" />
         </RouterLink>
 
-        <RouterLink :to="{ path: isAdmin() ? '/admin' : '/profile', query: { origin: ['Home'] } }">
-          <v-icon
-            :color="accountActive ? '#DBB671' : 'black'"
+        <RouterLink :to="{ path: isAdmin() ? '/admin' : '/profile' }">
+          <Icon
+            :color="accountActive ? 'golden' : 'black'"
             size="x-large"
+            tooltip="My Account"
             :icon="isAdmin() ? adminIcon : accountIcon" />
         </RouterLink>
-
-        <Button class="ml-5" title="SAIR" color="error" @click="router.push({ name: 'Login' })" />
       </v-row>
+    </v-col>
+    <v-col>
+      <RouterLink :to="{ path: '/login' }">
+        <Icon tooltip="Logout" color="red" size="x-large" icon="mdi-logout" />
+      </RouterLink>
     </v-col>
   </v-row>
 </template>
@@ -89,7 +81,7 @@
   import isAdmin from '../utils/isAdmin'
   import router from '@/core/router'
   import Input from '@/core/components/Input.vue'
-  import Button from '@/core/components/Button.vue'
+  import Icon from '@/core/components/Icon.vue'
   import CartMenu from '@/cart/components/CartMenu.vue'
 
   const props = defineProps({
@@ -110,17 +102,15 @@
       default: false,
     },
   })
-
-  // const prod1 = ref({
-  //   discount: 35,
-  //   name: "Laptop",
-  //   price: "960",
-  //   fullPrice: "1160",
-  //   isInWishList: true,
-  // })
-
   const accountIcon = props.accountActive ? 'mdi-account-circle-outline' : 'mdi-account-outline'
   const adminIcon = props.accountActive ? 'mdi-shield-account' : 'mdi-shield-account-outline'
+
+  const categories = [
+    { name: 'Mais Vendidos', path: '/store', category: 'most-seller' },
+    { name: 'Eletrônicos', path: '/store', category: 'eletronics' },
+    { name: 'Domésticos', path: '/store', category: 'household' },
+    { name: 'Livros', path: '/store', category: 'books' },
+  ]
 
   const showCart = ref(false)
   const dadoPesquisa = ref(null)
---
+++ vendi_frontend/src/core/components/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-tooltip v-if="tooltip" location="top">
+    <template #activator="{ props: tooltipProps }">
+      <v-icon :color="color" v-bind="tooltipProps" :size="size" :icon="icon" />
+    </template>
+    <span>{{ tooltip }}</span>
+  </v-tooltip>
+
+  <v-icon v-else :color="color" :size="size" :icon="icon" />
+</template>
+
+<script setup>
+  const props = defineProps({
+    size: { type: String, default: null },
+    icon: { type: String, required: true },
+    tooltip: { type: String, default: null },
+    color: { type: String, default: null },
+  })
+</script>
