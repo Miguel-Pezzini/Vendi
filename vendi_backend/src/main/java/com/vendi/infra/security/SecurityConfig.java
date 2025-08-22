4f192bbd9089fab8379bf2ab49803f2043c51ec1
Miguel-Pezzini <miguel.kuhr@gmail.com>
2025-08-21 21:21:09 -0300

    wip: Integration with Prometheus + Grafana

--- vendi_backend/src/main/java/com/vendi/infra/security/SecurityConfig.java
+++ vendi_backend/src/main/java/com/vendi/infra/security/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/actuator/prometheus").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
