76b6cdac38c55d71cb88c9258610d4d258797c83
Miguel Pezzini Kuhr <miguel.kuhr@gmail.com>
2025-09-01 11:28:33 -0300

    wip: Trying to fix auth test bug

--- vendi_backend/src/test/java/com/vendi/integration/AbstractIntegrationTest.java
+++ vendi_backend/src/test/java/com/vendi/integration/AbstractIntegrationTest.java
@@ -1,6 +1,9 @@
 package com.vendi.integration;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -8,21 +11,41 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 public abstract class AbstractIntegrationTest {
 
     @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
-            .withDatabaseName("vendi")
-            .withUsername("user")
-            .withPassword("1234");
+    private static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16")
+                    .withDatabaseName("vendi")
+                    .withUsername("user")
+                    .withPassword("1234");
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeEach
+    void cleanDatabase() {
+        jdbcTemplate.execute("""
+            DO
+            $func$
+            BEGIN
+                EXECUTE
+                (SELECT 'TRUNCATE TABLE ' || string_agg(format('%I.%I', schemaname, tablename), ', ')
+                        || ' RESTART IDENTITY CASCADE'
+                 FROM pg_tables
+                 WHERE schemaname = 'public');
+            END
+            $func$;
+        """);
     }
 }
