05d3fa1ff9d267bf333b1fa64d79c3e33993921d
Miguel-Pezzini <miguel.kuhr@gmail.com>
2026-03-12 19:45:59 -0300

    feat: Auth login and register responsive

--- vendi_frontend/src/authenticate/views/Login.vue
+++ vendi_frontend/src/authenticate/views/Login.vue
@@ -1,61 +1,54 @@
 <template>
-  <div class="d-flex flex-column min-h-screen">
-    <div class="d-flex align-center w-100 justify-space-between">
-      <img v-if="$vuetify.display.mdAndUp" src="../../assets/side-image-login.png" />
+  <div class="auth-page">
+    <main class="auth-shell">
+      <section v-if="$vuetify.display.mdAndUp" class="auth-visual" aria-hidden="true">
+        <div class="auth-visual__image-wrapper">
+          <img class="auth-visual__image" src="../../assets/side-image-login.png" alt="" />
+        </div>
+      </section>
 
-      <v-form
-        ref="form"
-        :class="[
-          'd-flex flex-column justify-center align-center w-100',
-          $vuetify.display.mdAndUp ? 'mx-16' : 'my-12 mx-6',
-        ]"
-        @submit.prevent="logar">
-        <div class="w-100" style="max-width: 380px">
-          <div
-            class="d-flex flex-column gap-6"
-            :class="{ 'text-center': !$vuetify.display.mdAndUp }">
-            <h1 class="font-weight-medium text-h4">Log In</h1>
-            <p>Enter your details below</p>
+      <section class="auth-content">
+        <v-form ref="form" class="auth-card" @submit.prevent="logar">
+          <div class="auth-copy">
+            <span class="auth-eyebrow">Welcome back</span>
+            <h1 class="auth-title">Log In</h1>
+            <p class="auth-subtitle">Enter your details below</p>
           </div>
 
-          <div class="d-flex flex-column mt-6 w-100">
-            <Input v-model="email" label="Enter your E-mail" required />
+          <div class="auth-fields">
+            <Input v-model="email" class="auth-input" label="Enter your E-mail" required />
             <Input
               v-model="password"
+              class="auth-input"
               append-icon="mdi-eye"
               label="Senha"
               required
               type="password" />
           </div>
 
-          <div
-            class="mt-6 d-flex align-center"
-            :class="$vuetify.display.mdAndUp ? 'justify-space-between' : 'justify-start gap-3'">
+          <div class="auth-actions">
             <v-btn
+              class="auth-submit"
               height="52"
-              :size="$vuetify.display.mdAndUp ? 'large' : 'default'"
-              :class="$vuetify.display.mdAndUp ? 'px-12 py-4' : 'px-6 py-3'"
               type="submit"
               color="golden"
               text="Login"
               :loading="!!$loadingState['auth/post']" />
             <v-btn
+              class="auth-secondary-action text-none"
               color="black"
               variant="text"
-              size="default"
-              text="Forgot your password?"
-              class="text-none" />
+              text="Forgot your password?" />
           </div>
 
-          <div class="d-flex justify-center align-center mt-4">
-            <RouterLink class="text-decoration-none text-black opacity-80" to="/register">
+          <div class="auth-link-row">
+            <RouterLink class="auth-link" to="/register">
               Don't have an account? <span class="text-golden">Create one!</span>
             </RouterLink>
           </div>
-        </div>
-      </v-form>
-    </div>
-
+        </v-form>
+      </section>
+    </main>
     <Footer />
   </div>
 </template>
@@ -66,6 +59,7 @@
   import Input from '@/core/components/Input'
   import Footer from '@/core/components/Footer.vue'
   import api from '@/core/plugins/api'
+  import '@/authenticate/styles/auth-layout.css'
 
   const { proxy } = getCurrentInstance()
 
--- vendi_frontend/src/authenticate/views/Register.vue
+++ vendi_frontend/src/authenticate/views/Register.vue
@@ -1,28 +1,32 @@
 <template>
-  <div class="d-flex flex-column min-h-screen">
-    <div class="d-flex w-100 align-center justify-space-between">
-      <img v-if="$vuetify.display.mdAndUp" src="../../assets/side-image-login.png" />
+  <div class="auth-page">
+    <main class="auth-shell">
+      <section v-if="$vuetify.display.mdAndUp" class="auth-visual" aria-hidden="true">
+        <div class="auth-visual__image-wrapper">
+          <img class="auth-visual__image" src="../../assets/side-image-login.png" alt="" />
+        </div>
+      </section>
 
-      <v-form
-        ref="form"
-        :class="[
-          'd-flex flex-column justify-center align-center w-100',
-          $vuetify.display.mdAndUp ? 'mx-16' : 'my-12 mx-6',
-        ]"
-        @submit.prevent="register">
-        <div class="w-100" style="max-width: 380px">
-          <div
-            class="d-flex flex-column gap-6"
-            :class="{ 'text-center': !$vuetify.display.mdAndUp }">
-            <h1 class="font-weight-medium text-h4">Register your account</h1>
-            <p>Enter your details below</p>
+      <section class="auth-content">
+        <v-form ref="form" class="auth-card auth-card--register" @submit.prevent="register">
+          <div class="auth-copy">
+            <span class="auth-eyebrow">Create your account</span>
+            <h1 class="auth-title">Register your account</h1>
+            <p class="auth-subtitle">Enter your details below</p>
           </div>
 
-          <div class="d-flex flex-column mt-6 w-100">
-            <Input v-model="name" label="Name" required />
-            <Input v-model="email" label="E-mail" required validate-on="blur" :rules="emailRules" />
+          <div class="auth-fields">
+            <Input v-model="name" class="auth-input" label="Name" required />
+            <Input
+              v-model="email"
+              class="auth-input"
+              label="E-mail"
+              required
+              validate-on="blur"
+              :rules="emailRules" />
             <Input
               v-model="password"
+              class="auth-input"
               append-icon="mdi-eye"
               label="Password"
               required
@@ -31,6 +35,7 @@
               type="password" />
             <Input
               v-model="repeatPassword"
+              class="auth-input"
               append-icon="mdi-eye"
               :rules="passwordRules"
               label="Repeat your password"
@@ -39,27 +44,24 @@
               type="password" />
           </div>
 
-          <div class="mt-6 d-flex align-center">
+          <div class="auth-actions auth-actions--stacked">
             <v-btn
+              class="auth-submit"
               height="52"
-              :size="$vuetify.display.mdAndUp ? 'large' : 'default'"
-              :class="$vuetify.display.mdAndUp ? 'px-12 py-4' : 'px-6 py-3'"
               type="submit"
-              class="w-100"
               color="golden"
               :loading="!!$loadingState['auth/post']"
               text="Register account" />
           </div>
 
-          <div class="d-flex justify-center align-center mt-4">
-            <RouterLink class="text-decoration-none text-black opacity-80" to="/login">
+          <div class="auth-link-row">
+            <RouterLink class="auth-link" to="/login">
               Alredy have an account? <span class="text-golden">Login!</span>
             </RouterLink>
           </div>
-        </div>
-      </v-form>
-    </div>
-
+        </v-form>
+      </section>
+    </main>
     <Footer />
   </div>
 </template>
@@ -70,6 +72,7 @@
   import Input from '@/core/components/Input'
   import Footer from '@/core/components/Footer.vue'
   import api from '@/core/plugins/api'
+  import '@/authenticate/styles/auth-layout.css'
 
   const { proxy } = getCurrentInstance()
 
